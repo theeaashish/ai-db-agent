c59b0c180e5813506d0393ad0e0fa2307df813e8
Aashish Joshi <theeaashish@gmail.com>
2026-02-14 21:37:03 +0530

    feat: add date format

---
+++ lib/formatDate.ts
@@ -0,0 +1,8 @@
+// Format date into readable string.
+
+export function formatDate(date: Date) {
+  return new Intl.DateTimeFormat("en-IN", {
+    dateStyle: "medium",
+    timeStyle: "short",
+  }).format(date);
+}
